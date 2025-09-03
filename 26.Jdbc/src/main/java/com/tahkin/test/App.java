7b20551b39a886b0312a8866a596237c45d64f9f
Nikhat Parvin <nikhat.parvin013@gmail.com>
2025-09-03 17:05:50 +0530

    second commit

--- 26.Jdbc/src/main/java/com/tahkin/test/App.java
+++ 26.Jdbc/src/main/java/com/tahkin/test/App.java
@@ -1,54 +1,64 @@
 package com.tahkin.test;
-// This is the package declaration (defines the namespace for your class)
+// Package declaration (defines the namespace for this class)
 
-import java.sql.Connection;   // Importing Connection interface for connecting to the database
-import java.sql.DriverManager; // DriverManager is used to create database connections
-import java.sql.ResultSet;     // ResultSet is used to store and access the results of SQL queries
-import java.sql.Statement;     // Statement is used to send SQL queries to the database
+// Importing required JDBC classes
+import java.sql.Connection;   // Connection object is used to connect Java app with DB
+import java.sql.DriverManager; // DriverManager helps to establish a DB connection
+import java.sql.ResultSet;     // ResultSet holds the data retrieved from SQL queries
+import java.sql.Statement;     // Statement is used to send SQL queries to DB
 
 /**
- * This program connects to a MySQL database using JDBC
- * and retrieves a student's name based on their ID.
+ * JDBC Example Program:
+ * ---------------------
+ * - Connects to a MySQL database
+ * - Executes a query on the "students" table
+ * - Retrieves and prints all student records (id, name, city)
  *
- * In Maven projects:
- * - You don’t need to manually add jar files.
- * - Adding dependency in `pom.xml` automatically downloads MySQL connector.
+ * Note:
+ * In Maven projects, you don’t need to manually add JAR files.
+ * Just add MySQL connector dependency in pom.xml.
  */
 public class App {
     public static void main(String[] args) throws Exception {
 
         // Database URL format: jdbc:mysql://hostname:port/databaseName
+        // "employees" is the database name
         String url = "jdbc:mysql://localhost:3306/employees";
 
-        // Username and password for MySQL database
-        String uname = "root";
-        String pass = "";
+        // MySQL database username and password
+        String uname = "root";       // your DB username
+        String pass = "";   // your DB password
 
-        // SQL query: selects "name" column from "students" table where id = 101
-        String query = "SELECT name FROM students WHERE id = 101;";
+        // SQL query: fetch all columns from the "students" table
+        String query = "SELECT * FROM students";
 
-        // Establish a connection to the database
+        // Establish connection with MySQL using DriverManager
         Connection con = DriverManager.getConnection(url, uname, pass);
 
         // Create a Statement object to execute queries
         Statement st = con.createStatement();
 
-        // Execute the query and store the result in ResultSet
+        // Execute the query and store results inside ResultSet
         ResultSet rs = st.executeQuery(query);
 
-        // Move the cursor to the first row in ResultSet
-        rs.next();
+        // Loop through the ResultSet (row by row)
+        while (rs.next()) {
+            // Fetch values from each column
+            // Indexes: 1 = id, 2 = name, 3 = city
+            int id = rs.getInt(1);       // get "id" column (1st column)
+            String name = rs.getString(2); // get "name" column (2nd column)
+            String city = rs.getString(3); // get "city" column (3rd column)
 
-        // Get the value of the "name" column from the current row
-        String name = rs.getString("name");
+            // Print the student record
+            System.out.println(id + " : " + name + " : " + city);
+        }
 
-        // Print the retrieved name on the console
-        System.out.println(name);
-
-        // Close the statement and release resources
+        // Close the statement to release resources
         st.close();
 
-        // NOTE: Always good practice to close connection too
+        // Close the connection to free database resources
         con.close();
+
+        // NOTE: Calling con.close() twice is not necessary, once is enough
     }
 }
